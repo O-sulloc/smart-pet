56191430afcf3df964ca192102d26a1fa574cfc0
keemzeehae <keemzeehae@naver.com>
2022-05-10 22:01:25 +0900

    ServiceVO ReservationVO

---
+++ src/main/java/com/pj/pet/reservation/ReservationVO.java
@@ -0,0 +1,17 @@
+package com.pj.pet.reservation;
+
+import java.sql.Date;
+
+import lombok.Data;
+@Data
+public class ReservationVO {
+
+	
+	 private Long resNum;
+	 private String id;
+	 private Long sNum;
+	 private Date resDate;
+	 private Date resTime;
+	 private String resMemo;
+	 private Long resState;
+}
---
+++ src/main/java/com/pj/pet/service/ServiceVO.java
@@ -0,0 +1,16 @@
+package com.pj.pet.service;
+
+import lombok.Data;
+
+@Data
+public class ServiceVO {
+	
+	private Long sNum;
+	private String sName;
+	private String sTel;
+	private String sAddress;
+	private String sTime;
+	private String hospitalField;
+	private String homepage;
+	private Long sKind;
+}
